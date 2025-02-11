4c032d5c8f6902d45b450a968845f0c21842a0fe
valentynliubchenko <luybchenko.val@gmail.com>
2025-02-11 23:22:20 +0200

    Second commit

--- src/main/java/ua/nure/MyApp.java
+++ src/main/java/ua/nure/MyApp.java
@@ -1,12 +1,13 @@
 package ua.nure;
 
-public class MyApp
-{
+public class MyApp {
     public static void main(String[] args) {
         if (args.length > 0) {
             System.out.println(generateGreeting(args[0]));
         } else {
+
             System.out.println("Hello world");
+
         }
     }
 
